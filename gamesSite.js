93a1119f52e918b30e8aba5dc237ab8288a99a97
Josh-Sapira <100649893+Josh-Sapira@users.noreply.github.com>
2023-07-19 10:23:17 +0300

    Remove text from buttons

--- gamesSite.js
+++ gamesSite.js
@@ -77,7 +77,6 @@ for(let i=0; i<6; i++) {
     const testButton = document.createElement('button');
     testButton.setAttribute('class', 'gameButton');
     testButton.style.backgroundColor = 'yellow';
-    testButton.innerHTML = "Coming Soon!";
     buttons.unshift(testButton);
 }
 loadButtons(middle);
